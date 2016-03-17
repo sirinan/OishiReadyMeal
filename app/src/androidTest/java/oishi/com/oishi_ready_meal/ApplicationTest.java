1c29eb47e68a4e823ac7eb128517350d614c85c5
sirinan <sirinan@edispro.com>
2016-03-17 18:20:08 +0700

    Create Project

---
+++ app/src/androidTest/java/oishi/com/oishi_ready_meal/ApplicationTest.java
@@ -0,0 +1,13 @@
+package oishi.com.oishi_ready_meal;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/oishi/com/oishi_ready_meal/C002_AdapterMenu.java
@@ -0,0 +1,30 @@
+package oishi.com.oishi_ready_meal;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Mimi on 3/17/16.
+ */
+public class C002_AdapterMenu extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
